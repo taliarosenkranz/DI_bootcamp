21cc2a6bc1a48b5a0e86d57d90c5e1379e056c01
Taliarosenkranz <talia@jaloucity.biz>
2024-03-18 22:26:59 +0200

    daily challenge done

---
+++ Week3_Database/Day_1/daily_challenge.sql
@@ -0,0 +1,5 @@
+-- 1. Count how many actors are in the table.
+select count(*)
+from actors
+-- 2. Try to add a new actor with some blank fields. What do you think the outcome will be ?
+--Also blank feelds count as values as they take up space in the computer too. 
